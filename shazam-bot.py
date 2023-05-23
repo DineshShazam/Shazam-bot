ad6d5d57e6608bbe3ef811ff3dd4c5b42d864e08
Dinesh-00111 <dinesh@acubetechnologies.in>
2023-05-23 22:21:57 +0530

    added password breach check and google translator

--- shazam-bot.py
+++ shazam-bot.py
@@ -1,14 +1,11 @@
 import logging as log
-import json
-from pathlib import Path
 import os
-import sys
-import requests
-from telegram import Update # python-telegram-bot
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+import telebot #pip install pyTelegramBotAPI
+from telebot import apihelper
+import os
 from dotenv import load_dotenv # python-dotenv
-import hashlib
-
+import utils
+import re
 
 log.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -18,93 +15,147 @@ log.basicConfig(
 load_dotenv('./.env')
 
 BOT_TOKEN = os.getenv('TELE_BOT_TOKEN')
-BOT_USERNAME = os.getenv('BOT_USERNAME')
-API_TOKEN = os.getenv('PUBLIC_API_TOKEN')
 
 
-#! try catch decorator
-def exception_handling(func):
-    def wrapper_func(*args,**kwargs):
-        try:
-            func(*args,*kwargs)
-        except Exception as e:
-            log.error(f'{func.__name__} failed to execute. {e}')
-            sys.exit()
-    
-    return wrapper_func
+#* loading bot 
+log.info('Bot started')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+#* force recreation after 5 minutes without any activity.
+apihelper.SESSION_TIME_TO_LIVE = 5 * 60
+
+#* responds to the initial message
+@bot.message_handler(commands=['start','hello'])
+def start_command_bot(message):
+    print(message.chat.type)
+    text = f'Hi {message.from_user.first_name}, I am Shazam, I am here to serve you. Please Type /help to look on the options'
+    bot.reply_to(message, text)
 
 
-async def start_command_bot(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = 'Hi, I am Shazam, I am here to serve you. Please Type /Help to look on the options'
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-
-async def help_command_bot(update: Update, context: ContextTypes.DEFAULT_TYPE):
+@bot.message_handler(commands=['help'])
+def help_command_bot(message):
     text = '''
         Type the option you prefer
-        /password ==> To check the given password has been breached or not.😀
+        /horoscope ==> To check on your horoscope. 
+        /password ==> To check the given password has been breached or not.
         /website ==> To check is the given domain has been hacked or not.
         /translator ==> Given Sentence will be translated to the specified language.
         /meaning ==> Will list the definition of the given word.
         /start ==> shazam will welcome you again. 
         /help ==> will list the above menu.
     '''
-    await context.bot.send_message(chat_id=update.effective_chat, text=text)
+    bot.send_message(message.chat.id, text)
 
-def handle_message(text : str) -> str:
-    message = text.lower()
+#* HOROSCOPE 
+# this function ask the question which sign needed from the list
+@bot.message_handler(commands=['horoscope'])
+def get_horoscope_sign_bot(message):
+     text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+     sent_msg = bot.send_message(message.chat.id,text, parse_mode='Markdown')
+     # triggers the next handler 
+     bot.register_next_step_handler(sent_msg,get_day_handler_bot)
 
-    if 'hey' in message:
-        return 'Hey 🖖 thanks for pinging me....'
+# this function will check the given sign is valid or not 
+# this function will ask for the day that need to be checked
+def get_day_handler_bot(message):
+    zodiac_list = ['Aries','Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+    zodiac_sign = message.text
+
+    # check the zodiac sign is valid or not 
+    zodiac_check = [var for var in zodiac_list if zodiac_sign.upper() in var.upper()]
     
-    if 'how are you' in message:
-        return 'I am fine 😊, I hope the same for you.. enjoy your day..'
+    if not zodiac_check:
+        text = f'Invalid Option Selected {zodiac_sign}, please type /horoscope to check again'
+        bot.reply_to(message, text)
+
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(message.chat.id,text)
+    bot.register_next_step_handler(sent_msg,fetch_horoscope_data,zodiac_sign)
+
+def fetch_horoscope_data(message,sign: str):
+    day = message.text
+    # if the day variable starts with number format check is the date valid or not
+    if re.match(r'^\d', day):
+       value = utils.is_valid_date(day)
+       if not value: bot.send_message(message.chat.id, "Date format is invalid it should be YYY-MM-DD!, please Type /horoscope to check again")
+    else:
+        if day.upper() not in ['TODAY', 'TOMORROW', 'YESTERDAY']:
+            bot.send_message(message.chat.id, "Invalid Option selected should be 'TODAY', 'TOMORROW', 'YESTERDAY'!, please Type /horoscope to check again")
     
-    return 'unknown message'
+    json_data = utils.get_horoscope_data(sign,day)
+    if not json_data : bot.send_message(message.chat.id, "Horoscope API is not working kinldy type /help and check on other options.."); return
+    data = json_data["data"]
+    horoscope_message = f'*Horoscope:* \n {data["horoscope_data"]} \n*Sign:* \n{sign} \n*Day:* \n{data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+    bot.send_message(message.chat.id, f"Type */help* to go to the menu",parse_mode='Markdown')
 
-async def unknow_message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_type : str = update.message.chat.type
-    message_text : str = update.message.text
+#* PASSWORD BREACH CHECK
+@bot.message_handler(commands=['password'])
+def password_command_bot(message):
+    text = f'Type the password which you want to check is it breached or not !'
+    msg = bot.send_message(message.chat.id,text)
+    bot.register_next_step_handler(msg, get_password_breach_count)
 
-    # if the bot has been mentioned in group this block will react
-    if message_type == 'group':
-        # remove the bot name from the text
-        if BOT_USERNAME in message_text:
-            new_text : str = message_text.replace(BOT_USERNAME,'').strip()
-            response : str = handle_message(new_text)
-        else:
-            return # We don't want the bot respond if it's not mentioned in the group
+def get_password_breach_count(message):
+    password_text = message.text
+    hashed_string = utils.hashing_text(password_text)
+    hashed_prefix,hashed_suffix = hashed_string[:5], hashed_string[5:]
+    
+    response = utils.get_password_breach_data(hashed_prefix)
+    if not response : bot.send_message(message.chat.id, "Password API is not working kinldy type /help and check on other options.."); return
+    
+    count = utils.password_breach_count(response,hashed_suffix)
+
+    if count:
+        bot.send_message(message.chat.id, f"*Password:* '{password_text}' has been breached *{count}* times",parse_mode='Markdown')
+        bot.send_message(message.chat.id, f"Click 👉 */help* to go to the menu or Click 👉 */password* to check again",parse_mode='Markdown')
     else:
-        response : str = handle_message(message_text)
+        bot.send_message(message.chat.id, f"*Password:* '{password_text}' has never been breached, Good To Go 👍",parse_mode='Markdown')
+        bot.send_message(message.chat.id, f"Click 👉 */help* to go to the menu or Click 👉 */password* to check again",parse_mode='Markdown')
 
-    if 'unknown message' in response:
-        await update.message.reply_text(f'I don\'t understand kinldy type /help in {BOT_USERNAME} to check the options')
+#* Translator
+@bot.message_handler(commands=['translator'])
+def translator_command_bot(message):
+    text = 'Enter the Text you want to translate'
+    msg = bot.send_message(message.chat.id,text)
+    bot.register_next_step_handler(msg,pick_language)
+
+def pick_language(message):
+    text_to_translate = message.text
+    text = f'''
+        Type the language code you want to translate 
+        * Type *ja* for Japanese
+        * Type *es* for Spanish
+        * Type *hi* for Hindi
+        * Type *de* for German
+        * Type *pt* for portuguese 
+    '''
+    msg = bot.send_message(message.chat.id,text)
+    bot.register_next_step_handler(msg,google_translate_text,text_to_translate)
+
+def google_translate_text(message, text_to_translate):
+    lang_code_to_translate = message.text.lower()
+    if lang_code_to_translate not in ['es','de','pt','ja','hi']:
+        bot.send_message(message.chat.id, f"Invalid language code given {lang_code_to_translate} Click 👉 */help* to go to the menu or Click 👉 */translator* to check again",parse_mode='Markdown')
+        return
+    
+    translated_response = utils.get_google_translate(text_to_translate,lang_code_to_translate)
+    transtated_text = translated_response['data']['translations'][0]['translatedText']
+
+    if transtated_text:
+        text = f'Given Text: *{text_to_translate}* \n Translated Text: *{transtated_text}*'
+        bot.send_message(message.chat.id,text)
+        bot.send_message(message.chat.id, f"Click 👉 */help* to go to the menu or Click 👉 */translator* to check again",parse_mode='Markdown')
     else:
-        await update.message.reply_text(response)
+        text = f'Given Text: *{text_to_translate}* \n *Failed* to translate'
+        bot.send_message(message.chat.id,text)
+        bot.send_message(message.chat.id, f"Click 👉 */help* to go to the menu or Click 👉 */translator* to check again",parse_mode='Markdown')
 
-# Log errors
-async def bot_error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log.error(f'bot failed {update} caused error {context.error}')
-    await update.message.reply_text(f'Sorry {update.effective_chat.first_name} request has been failed with Error {context.error} \n try to chat with bot after 2 mins...')
+@bot.message_handler(func=lambda msg : True)
+def echo_all_msg(message):
+    text = f'sorry {message.from_user.first_name}, I am not programmed for this command yet, Click 👉 */help* to go to the menu'
+    bot.reply_to(message, text)
 
-
-if __name__ == '__main__':
-
-    # build the app
-    log.info('Starting bot application...')
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-     # command
-    app.add_handler(CommandHandler('start',start_command_bot))
-    app.add_handler(CommandHandler('help',help_command_bot))
-
-    # message handler
-    app.add_handler(MessageHandler(filters.TEXT,unknow_message_handler))
-
-    # error handler
-    app.add_error_handler(bot_error_handler)
-
-
-    log.info('Polling....')
-    app.run_polling()
-
-
+log.info('Bot is in pooling mode')
+bot.infinity_polling()
---
+++ utils.py
@@ -0,0 +1,111 @@
+import requests
+from pathlib import Path
+import json
+import logging as log
+import sys
+import os
+import hashlib
+from dotenv import load_dotenv # python-dotenv
+from datetime import datetime
+
+load_dotenv('./.env')
+
+BOT_USERNAME = os.getenv('BOT_USERNAME')
+API_TOKEN = os.getenv('PUBLIC_API_TOKEN')
+
+#! try catch decorator
+def exception_handling(func):
+    def wrapper_func(*args,**kwargs):
+        try:
+            return func(*args,*kwargs)
+        except requests.exceptions.RequestException as e:
+            log.error(f'{func.__name__} API request failed to execute. {e}')
+            sys.exit()
+        except FileNotFoundError as e:
+            log.error(f'{func.__name__} File failed to execute. {e}')
+            sys.exit()
+        except ValueError:
+            log.error(f'{func.__name__} Invalid value format.')
+            sys.exit()
+        except Exception as e:
+            log.error(f'{func.__name__} failed to execute. {e}')
+            sys.exit()
+    
+    return wrapper_func
+
+#! url fetch function
+@exception_handling
+def get_url_by_type(type: str,url_key_name: str):
+    
+    url_file_path = Path('./url.json')
+
+    with url_file_path.open(mode='r') as file:
+        json_data = json.load(file)
+    
+    if type in json_data[0]:
+         url_data = json_data[0][type]
+    else: 
+        return False
+
+    if url_key_name in url_data:
+        url = url_data[url_key_name]   
+        return url
+    else:
+         return False
+    
+def is_valid_date(date_string):
+    try:
+        datetime.strptime(date_string, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+@exception_handling
+def hashing_text(password_text:str) -> str:
+    hashed_object = hashlib.sha1(password_text.encode('utf-8'))
+    hashed_string = hashed_object.hexdigest()
+    return hashed_string.upper()
+
+#* horoscope api
+@exception_handling
+def get_horoscope_data(sign: str, day: str) -> dict:
+    horoscope_url = get_url_by_type('public_api','horoscope_url')
+    if not horoscope_url : return False
+    params = {"sign": sign, "day": day.upper()}
+    response = requests.get(horoscope_url,params)
+    return response.json()
+
+@exception_handling
+def get_password_breach_data(hashed_password_preffix : str):
+    api_url = get_url_by_type('pwned','pwned_password_url')
+    if not api_url : return False
+    response = requests.get(f'{api_url}/{hashed_password_preffix}')
+    return response.text
+
+@exception_handling
+def password_breach_count(response, hashed_string: str) -> int:
+    hashed_response = [lines.split(':') for lines in response.splitlines()]
+    for hashed_response_suffix, count in hashed_response:
+        if hashed_response_suffix == hashed_string:
+           return count
+    return 0
+
+@exception_handling
+def get_google_translate(text_to_translate, lang_code_to_translate):
+    api_url = get_url_by_type('public_api','translator_url')
+  
+    payload = {
+        "q": text_to_translate,
+        "target": lang_code_to_translate,
+        "source": "en"
+    }
+
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "application/gzip",
+        "X-RapidAPI-Key": API_TOKEN,
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
+    }
+
+    response = requests.post(api_url,data=payload,headers=headers)
+    return response.json()
